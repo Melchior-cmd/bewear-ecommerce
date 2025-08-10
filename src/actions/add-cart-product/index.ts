295ab9c338aeb9dcca0bb74e53675978a3acf703
Lucas Melchior <lucas.melchior@dpu.def.br>
2025-08-10 17:09:19 -0300

    feat: add product to cart action

---
+++ src/actions/add-cart-product/index.ts
@@ -0,0 +1,64 @@
+"use server";
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+
+import { db } from "@/db";
+import { cartItemTable, cartTable, productVariantTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+
+import { AddProductToCartSchema, addProductToCartSchema } from "./schema";
+
+export const addProductToCart = async (data: AddProductToCartSchema) => {
+  addProductToCartSchema.parse(data);
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user) {
+    throw new Error("Unauthorized");
+  }
+
+  const produtVariant = await db.query.productVariantTable.findFirst({
+    where: (productVariant, { eq }) =>
+      eq(productVariant.id, data.producrtVariantId),
+  });
+
+  if (!produtVariant) {
+    throw new Error("Product variant not found");
+  }
+
+  const cart = await db.query.cartTable.findFirst({
+    where: (cart, { eq }) => eq(cart.userId, session.user.id),
+  });
+  let cartId = cart?.id;
+  if (!cart) {
+    const [newCart] = await db
+      .insert(cartTable)
+      .values({
+        userId: session.user.id,
+      })
+      .returning();
+    cartId = newCart.id;
+  }
+  const cartItem = await db.query.cartItemTable.findFirst({
+    where: (cartItem, { eq }) =>
+      eq(cartItem.cartId, cartId) &&
+      eq(cartItem.productVariantId, data.producrtVariantId),
+  });
+
+  if (cartItem) {
+    await db
+      .update(cartItemTable)
+      .set({
+        quantity: cartItem.quantity + data.quantity,
+      })
+      .where(eq(cartItemTable.id, cartItem.id));
+    return;
+  }
+  await db.insert(cartItemTable).values({
+    cartId,
+    productVariantId: data.producrtVariantId,
+    quantity: data.quantity,
+  });
+};
