5b28fc4291bd9e35b4706577ce85388c013d7ca2
Lucas Melchior <lucas.melchior@dpu.def.br>
2025-08-11 21:22:38 -0300

    feat: ensure product can be removed from cart

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
 import { ShoppingBasketIcon } from "lucide-react";
-import Image from "next/image";
 
 import { getCart } from "@/actions/get-cart";
 import { formatCentsToBRL } from "@/helpers/money";
