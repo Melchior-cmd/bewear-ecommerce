958bbef9f1f0b7ce76e9d3f9bbe68b6162d1a5ab
Lucas Melchior <lucas.melchior@dpu.def.br>
2025-08-25 17:53:40 -0300

    chore: change name the import

--- src/hooks/queries/use-user-addresses.ts
+++ src/hooks/queries/use-user-addresses.ts
@@ -1,12 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 
 import { getUserAddresses } from "@/actions/get-user-addresses";
-import { shippingAddressTable } from "@/db/schema";
+import { ShippingAddressTable } from "@/db/schema";
 
 export const getUserAddressesQueryKey = () => ["user-addresses"] as const;
 
 export const useUserAddresses = (params?: {
-  initialData?: (typeof shippingAddressTable.$inferSelect)[];
+  initialData?: (typeof ShippingAddressTable.$inferSelect)[];
 }) => {
   return useQuery({
     queryKey: getUserAddressesQueryKey(),
